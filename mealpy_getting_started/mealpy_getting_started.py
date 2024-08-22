37d380496d57f468475ff3b813737d03ec152ff8
Gabriel Malosto <professional@gabdumal.com>
2024-08-22 01:27:33 -0300

    Use Opfunu function

--- mealpy_getting_started/mealpy_getting_started.py
+++ mealpy_getting_started/mealpy_getting_started.py
@@ -1,13 +1,15 @@
 from mealpy import FloatVar, GA
-import numpy as np
+from opfunu.cec_based.cec2014 import F12014
+
+objective_function = F12014(ndim=30)
 
 
-def objective_function(solution):
-    return np.sum(solution**2)
+def objective_function_wrapper(solution):
+    return objective_function.evaluate(solution)
 
 
 problem_dictionary = {
-    "obj_func": objective_function,
+    "obj_func": objective_function_wrapper,
     "bounds": FloatVar(
         lb=([-100] * 30),
         ub=([100] * 30),
